d5713b83520acb7a681e3795a9c2b788f7387bd8
Mark Heramis <mark@getdentalray.com>
2023-11-03 11:01:52 +0800

    update better ServerAsyncWorker
    Signed-off-by: Mark Heramis <mark@getdentalray.com>

--- src/ServerAsyncWorker.cc
+++ src/ServerAsyncWorker.cc
@@ -16,8 +16,8 @@ using json = nlohmann::json;
 #include "dcmtk/dcmnet/diutil.h"
 #include "dcmtk/dcmdata/dcdict.h"
 #include "dcmtk/dcmqrdb/dcmqrsrv.h"
-#include "dcmtk/dcmqrdb/dcmqrcbf.h" 
-#include "dcmtk/dcmqrdb/dcmqrcbm.h" 
+#include "dcmtk/dcmqrdb/dcmqrcbf.h"
+#include "dcmtk/dcmqrdb/dcmqrcbm.h"
 #include "dcmtk/dcmqrdb/dcmqrcbg.h"
 #include "dcmtk/dcmqrdb/dcmqrcbs.h"
 #include "dcmtk/dcmqrdb/dcmqropt.h"
@@ -27,137 +27,189 @@ using json = nlohmann::json;
 
 ServerAsyncWorker::ServerAsyncWorker(std::string data, Function &callback) : BaseAsyncWorker(data, callback)
 {
-    ns::registerCodecs();
+  ns::registerCodecs();
 }
 
 void ServerAsyncWorker::Execute(const ExecutionProgress &progress)
 {
-  ns::sInput in = ns::parseInputJson(_input);
-
-  EnableVerboseLogging(in.verbose);
-
-  if (!in.source.valid())
-  {
-    SetErrorJson("Source not set");
+  ns::sInput in;
+  if (!InitializeAndValidateInput(in, progress))
     return;
-  }
-
-  if (in.storagePath.empty())
-  {
-    in.storagePath = "./data";
-    SendInfo("storage path not set, defaulting to " + in.storagePath, progress);
-  }
-
-  int opt_port = in.source.port;
-  static OFString opt_outputDirectory =  OFString(in.storagePath.c_str());
-
+  if (!HandleDirectory(in))
+    return;
   T_ASC_Network *net;
-  DcmAssociationConfiguration asccfg;
-
-  OFStandard::initializeNetwork();
-
-  /* make sure data dictionary is loaded */
-  if (!dcmDataDict.isDictionaryLoaded())
+  if (!InitializeNetwork(in, net))
+    return;
+  if (in.storeOnly)
   {
-    SetErrorJson(std::string("No data dictionary loaded"));
+    if (!HandleStoreOnly(in, net, progress))
+      return;
   }
-
-  /* if the output directory does not equal "." (default directory) */
-  if (opt_outputDirectory != ".")
+  else
   {
-    /* if there is a path separator at the end of the path, get rid of it */
+    if (!HandleQueryRetrieve(in, net))
+      return;
+  }
+  ShutdownNetwork(net);
+}
+bool ServerAsyncWorker::InitializeAndValidateInput(ns::sInput &in, const ExecutionProgress &progress)
+{
+    in = ns::parseInputJson(_input);
+    EnableVerboseLogging(in.verbose);
+    if (!in.source.valid())
+    {
+        SetErrorJson("Source not set");
+        return false;
+    }
+    if (in.storagePath.empty())
+    {
+        in.storagePath = "./data";
+        SendInfo("storage path not set, defaulting to " + in.storagePath, progress);
+    }
+    return true;
+}
+bool ServerAsyncWorker::HandleDirectory(ns::sInput &in)
+{
+    // Check if the data dictionary is loaded
+    if (!dcmDataDict.isDictionaryLoaded())
+    {
+        SetErrorJson(std::string("No data dictionary loaded"));
+        return false;
+    }
+
+    static OFString opt_outputDirectory = OFString(in.storagePath.c_str());
+
+    // Normalize the directory name
     OFStandard::normalizeDirName(opt_outputDirectory, opt_outputDirectory);
 
-    /* check if the specified directory exists and if it is a directory.
-     * If the output directory is invalid, dump an error message and terminate execution.
-     */
-    if (!OFStandard::dirExists(opt_outputDirectory))
+    // Check if the specified directory exists
+    if (opt_outputDirectory != "." && !OFStandard::dirExists(opt_outputDirectory))
     {
-      SetErrorJson(std::string("Specified output directory does not exist: ") + std::string(opt_outputDirectory.c_str()));
-      return;
+        SetErrorJson(std::string("Specified output directory does not exist: ") + std::string(opt_outputDirectory.c_str()));
+        return false;
     }
-  }
 
-  /* check if the output directory is writeable */
-  if (!OFStandard::isWriteable(opt_outputDirectory))
-  {
-    SetErrorJson(std::string("Specified output directory is not writeable: ") + std::string(opt_outputDirectory.c_str()));
-    return;
-  }
+    // Check if the output directory is writeable
+    if (!OFStandard::isWriteable(opt_outputDirectory))
+    {
+        SetErrorJson(std::string("Specified output directory is not writeable: ") + std::string(opt_outputDirectory.c_str()));
+        return false;
+    }
 
-  /* initialize network, i.e. create an instance of T_ASC_Network*. */
-  OFCondition cond = ASC_initializeNetwork(NET_ACCEPTOR, opt_port, 30, &net);
-  if (cond.bad())
-  {
-    SetErrorJson(std::string("Cannot create network: ") + std::string(cond.text()));
-    return;
-  }
+    return true;
+}
 
-  /* drop root privileges now and revert to the calling user id (if we are running as setuid root) */
-  if (OFStandard::dropPrivileges().bad())
-  {
-    SetErrorJson(std::string("setuid() failed, maximum number of threads for uid already running"));
-    return;
-  }
 
-  T_ASC_Network* network = NULL;
-  cond = ASC_initializeNetwork(NET_REQUESTOR, 0, 10000, &network);
 
-  if (cond.bad()) {
-      OFString temp_str;
-      DCMNET_ERROR("Failed to create requestor network: " << DimseCondition::dump(temp_str, cond));
-      return;
-  }
-  if (in.storeOnly) {
-      RetrieveScp scp(opt_outputDirectory, in.source.aet.c_str(), in.writeFile);
-      while (cond.good()) {
-          cond = scp.waitForAssociation(net, progress);
-      }
-  }
-  else {
-      DcmQueryRetriveConfigExt cfg;
-      for (auto peer: in.peers) {
-          cfg.addPeer(peer.aet.c_str(), peer.ip.c_str(), peer.port);
-      }
-      cfg.setStorageArea(in.storagePath.c_str());
-      cfg.setPermissiveMode(in.permissive);
- 
-      DcmQueryRetrieveOptions options;
-      options.net_ = network;
-      options.allowShutdown_ = true;
-      options.disableGetSupport_ = true;
-      options.maxAssociations_ = 128;
-      DcmXfer netTransPrefer = in.netTransferPrefer.empty() ? DcmXfer(EXS_Unknown) : DcmXfer(in.netTransferPrefer.c_str());
-      DcmXfer netTransPropose = in.netTransferPropose.empty() ? DcmXfer(EXS_Unknown) : DcmXfer(in.netTransferPropose.c_str());
-      DcmXfer writeTrans = in.writeTransfer.empty() ? DcmXfer(EXS_Unknown) : DcmXfer(in.writeTransfer.c_str());
-  
-      DCMNET_INFO("preferred (accepted) network transfer syntax for incoming associations: " << netTransPrefer.getXferName());
-      DCMNET_INFO("proposed network transfer syntax for outgoing associations: " << netTransPropose.getXferName());
-      DCMNET_INFO("write transfer syntax (recompress if different to accepted ts): " << writeTrans.getXferName());
-      DCMNET_INFO("max associations: " << options.maxAssociations_);
-      DCMNET_INFO("permissive mode: " << in.permissive);
+bool ServerAsyncWorker::InitializeNetwork(const ns::sInput &in, T_ASC_Network *&net)
+{
+    OFStandard::initializeNetwork();
 
-      options.networkTransferSyntax_ = netTransPrefer.getXfer();
-      options.networkTransferSyntaxOut_ = netTransPropose.getXfer();
-      options.writeTransferSyntax_ = writeTrans.getXfer();
+    int opt_port = in.source.port;
 
-      DcmQueryRetrieveSQLiteDatabaseHandleFactory factory(&cfg);
-      DcmAssociationConfiguration associationConfiguration;
+    // Initialize network as an acceptor
+    OFCondition cond = ASC_initializeNetwork(NET_ACCEPTOR, opt_port, 30, &net);
+    if (cond.bad())
+    {
+        SetErrorJson(std::string("Cannot create network: ") + std::string(cond.text()));
+        return false;
+    }
 
-      DcmQueryRetrieveSCP scp(cfg, options, factory, associationConfiguration);
-      while (cond.good()) {
-          cond = scp.waitForAssociation(net);
-      }
-  }
-    
-  /* drop the network, i.e. free memory of T_ASC_Network* structure. This call */
-  /* is the counterpart of ASC_initializeNetwork(...) which was called above. */
-  cond = ASC_dropNetwork(&net);
-  if (cond.bad())
-  {
-    SetErrorJson(std::string(cond.text()));
-    return;
-  }
+    // Drop root privileges for security (if running as setuid root)
+    if (OFStandard::dropPrivileges().bad())
+    {
+        SetErrorJson(std::string("setuid() failed, maximum number of threads for uid already running"));
+        return false;
+    }
 
-  OFStandard::shutdownNetwork();
+    return true;
+}
+
+
+bool ServerAsyncWorker::HandleStoreOnly(const ns::sInput &in, T_ASC_Network *net, const ExecutionProgress &progress)
+{
+    // Set the storage directory and parameters for the Service Class Provider (SCP)
+    static OFString opt_outputDirectory = OFString(in.storagePath.c_str());
+    RetrieveScp scp(opt_outputDirectory, in.source.aet.c_str(), in.writeFile);
+
+    // Initialize condition for association handling
+    OFCondition cond = EC_Normal;  // Initialize to normal condition for loop entry
+
+    // Continuously wait for incoming associations and process them
+    while (cond.good())
+    {
+        cond = scp.waitForAssociation(net, progress);
+        if (cond.bad())
+        {
+            // Handle the error or log it
+            DCMNET_ERROR("Failed to handle incoming association: " << cond.text());
+        }
+    }
+
+    // For simplicity, we return true if the condition remains good after handling all associations
+    return cond.good();
+}
+
+
+bool ServerAsyncWorker::HandleQueryRetrieve(const ns::sInput &in, T_ASC_Network *net)
+{
+    DcmQueryRetriveConfigExt cfg;
+
+    // Add peers to the configuration
+    for (auto peer: in.peers) 
+    {
+        cfg.addPeer(peer.aet.c_str(), peer.ip.c_str(), peer.port);
+    }
+
+    cfg.setStorageArea(in.storagePath.c_str());
+    cfg.setPermissiveMode(in.permissive);
+
+    DcmQueryRetrieveOptions options;
+    T_ASC_Network* network = NULL;
+
+    // Initialize the network as a requestor
+    OFCondition cond = ASC_initializeNetwork(NET_REQUESTOR, 0, 10000, &network);
+    if (cond.bad()) 
+    {
+        OFString temp_str;
+        DCMNET_ERROR("Failed to create requestor network: " << DimseCondition::dump(temp_str, cond));
+        return false;
+    }
+
+    options.net_ = network;
+    options.allowShutdown_ = true;
+    options.disableGetSupport_ = true;
+    options.maxAssociations_ = 128;
+
+    // Set transfer syntax preferences
+    DcmXfer netTransPrefer = in.netTransferPrefer.empty() ? DcmXfer(EXS_Unknown) : DcmXfer(in.netTransferPrefer.c_str());
+    DcmXfer netTransPropose = in.netTransferPropose.empty() ? DcmXfer(EXS_Unknown) : DcmXfer(in.netTransferPropose.c_str());
+    DcmXfer writeTrans = in.writeTransfer.empty() ? DcmXfer(EXS_Unknown) : DcmXfer(in.writeTransfer.c_str());
+
+    options.networkTransferSyntax_ = netTransPrefer.getXfer();
+    options.networkTransferSyntaxOut_ = netTransPropose.getXfer();
+    options.writeTransferSyntax_ = writeTrans.getXfer();
+
+    // Create SCP for query and retrieve
+    DcmQueryRetrieveSQLiteDatabaseHandleFactory factory(&cfg);
+    DcmAssociationConfiguration associationConfiguration;
+    DcmQueryRetrieveSCP scp(cfg, options, factory, associationConfiguration);
+
+    while (cond.good()) 
+    {
+        cond = scp.waitForAssociation(net);
+    }
+
+    return cond.good();
+}
+
+void ServerAsyncWorker::ShutdownNetwork(T_ASC_Network *&net)
+{
+    // Network shutdown logic
+    OFCondition cond = ASC_dropNetwork(&net);
+    if (cond.bad())
+    {
+        SetErrorJson(std::string(cond.text()));
+    }
+    OFStandard::shutdownNetwork();
 }
--- src/ServerAsyncWorker.h
+++ src/ServerAsyncWorker.h
@@ -6,9 +6,81 @@ using namespace Napi;
 
 class ServerAsyncWorker : public BaseAsyncWorker
 {
-    public:
-        ServerAsyncWorker(std::string data, Function &callback);
+public:
+    ServerAsyncWorker(std::string data, Function &callback);
+    /**
+     * @brief Orchestrates the main operations of the ServerAsyncWorker.
+     *
+     * Initiates the server asynchronous worker by parsing the input and setting up the logging. The method then goes through a series of initializations, validations, and operational steps, such as directory handling, network setup, and either storing or querying and retrieving DICOM data. Each step is designed to return early if it encounters issues, ensuring a fail-fast mechanism. The DICOM network is shut down gracefully at the end.
+     *
+     * @param progress A reference to the execution progress indicator, used for reporting the progress during the server worker's operation.
+     */
+    void Execute(const ExecutionProgress &progress);
 
-        void Execute(const ExecutionProgress& progress);
-
+private:
+    /**
+     * @brief Initializes the provided input parameters, ensures the source is valid, and sets a default storage path if none is specified.
+     *
+     * If the source information is not valid, an error message "Source not set" is set. If a storage path is not provided, it defaults to "./data", and an informational message is logged regarding this defaulting.
+     *
+     * @param in A constant reference to the input structure.
+     * @param progress A reference to the execution progress indicator.
+     *
+     * @return Returns 'true' if the initialization and validation succeed. Otherwise, returns 'false' and sets an appropriate error message.
+     */
+    bool InitializeAndValidateInput(ns::sInput &in, const ExecutionProgress &progress);
+    /**
+     * @brief Manages and validates directory-related configurations for the server worker.
+     *
+     * This method ensures that the DICOM data dictionary is loaded, validates the specified output directory, and checks its write permissions. If the output directory is not specified, it defaults to the current directory ("."). If the provided directory does not exist or is not writeable, appropriate error messages are set.
+     *
+     * @param in A reference to the input structure, which contains directory-related configurations.
+     *
+     * @return Returns 'true' if all directory-related validations pass. Otherwise, returns 'false' and sets an error message.
+     */
+    bool HandleDirectory(ns::sInput &in);
+    /**
+     * @brief Initializes the DICOM network and sets up necessary configurations.
+     *
+     * This method is responsible for initializing the DICOM network based on the provided input parameters. It starts by initializing the network and then attempts to set up an acceptor on a specified port. Furthermore, it ensures privilege dropping for security and tries to set up a requestor network.
+     *
+     * In case any step fails, relevant error messages are set, making it easier to diagnose issues during the network initialization process.
+     *
+     * @param in A constant reference to the input structure containing network configurations.
+     * @param net A reference to a pointer of the T_ASC_Network type, which will be initialized by this method.
+     *
+     * @return Returns 'true' if the network initialization is successful. Otherwise, returns 'false' and sets an error message.
+     */
+    bool InitializeNetwork(const ns::sInput &in, T_ASC_Network *&net);
+    /**
+     * @brief Manages the storing of DICOM data when operating in "store only" mode.
+     *
+     * This method facilitates the storage of DICOM data by setting up the appropriate SCP (Service Class Provider) using the provided storage path and AE title. It then continuously waits for incoming associations and processes them until a termination condition is met.
+     *
+     * @param in A constant reference to the input structure containing storage configurations and other related parameters.
+     * @param net A pointer to the initialized T_ASC_Network.
+     * @param progress A reference to the execution progress indicator, used for reporting progress during the storage process.
+     *
+     * @return Returns 'true' when the method completes its operation.
+     */
+    bool HandleStoreOnly(const ns::sInput &in, T_ASC_Network *net, const ExecutionProgress &progress);
+    /**
+     * @brief Manages the querying and retrieving of DICOM data when not operating in "store only" mode.
+     *
+     * This method sets up configurations for querying and retrieving DICOM data. It initializes the appropriate configurations, transfer syntax preferences, and SCP (Service Class Provider) settings using provided input. The method also logs essential information about transfer syntax and operational mode. It then continuously waits for and processes incoming associations until a termination condition is met.
+     *
+     * @param in A constant reference to the input structure containing query, retrieve, and other related parameters.
+     * @param net A pointer to the initialized T_ASC_Network.
+     *
+     * @return Returns 'true' when the method completes its operation.
+     */
+    bool HandleQueryRetrieve(const ns::sInput &in, T_ASC_Network *net);
+    /**
+     * @brief Gracefully shuts down the DICOM network.
+     *
+     * This method attempts to drop the established DICOM network and ensures proper cleanup. If any issues arise during the network shutdown, appropriate error messages are set for further diagnosis.
+     *
+     * @param net A reference to a pointer of the T_ASC_Network type, which represents the established DICOM network. This pointer will be nullified after the network is dropped.
+     */
+    void ShutdownNetwork(T_ASC_Network *&net);
 };
